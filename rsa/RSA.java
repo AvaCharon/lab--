a900430c0158e9a5a3fdde7738a318d5d2ec89ef
AvaCharon <yjffffz@gmail.com>
2022-10-12 14:35:06 +0800

    lab2:finish

--- rsa/RSA.java
+++ rsa/RSA.java
@@ -12,18 +12,16 @@ public class RSA {
     private static BigInteger[] PriKey = new BigInteger[2];
 
     public static void getKeyPair() {
-        BigInteger p = BigInteger.probablePrime(10,new Random());
-        BigInteger q = BigInteger.probablePrime(10,new Random());
+        BigInteger p = BigInteger.probablePrime(1024,new Random());
+        BigInteger q = BigInteger.probablePrime(1024,new Random());
         BigInteger n = p.multiply(q);
         BigInteger Eun = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
-        BigInteger e = BigInteger.probablePrime(4,new Random());
+        BigInteger e = BigInteger.probablePrime(128,new Random());
         BigInteger d = BigInteger.ONE;
         while (true) {
             BigInteger[] temp = ex_gcd(e,Eun);
-            if(temp[0].compareTo(BigInteger.ONE)==0){
+            if(temp[0].compareTo(BigInteger.ONE)==0&&temp[1].compareTo(BigInteger.ZERO)==1){
                 d = temp[1];
-                for(int i=0;i<3;i++)
-                    System.out.println(temp[i]);
                 break;
             }
             e = e.add(BigInteger.ONE);
@@ -32,11 +30,12 @@ public class RSA {
         PubKey[1]=n;
         PriKey[0]=d;
         PriKey[1]=n;
-        System.out.println(Eun.toString());
-        System.out.println(PubKey[0].toString());
-        System.out.println(PubKey[1].toString());
-        System.out.println(PriKey[0].toString());
-        System.out.println(PriKey[1].toString());
+        System.out.println("p="+p.toString());
+        System.out.println("q="+q.toString());
+        System.out.println("$(n)="+Eun.toString());
+        System.out.println("e="+PubKey[0].toString());
+        System.out.println("n="+PubKey[1].toString());
+        System.out.println("d="+PriKey[0].toString());
     }
 
     public static BigInteger[] ex_gcd(BigInteger a, BigInteger b, BigInteger[] x, BigInteger[] y){
@@ -77,12 +76,11 @@ public class RSA {
             }
             for(int j=0, i=0;i<ret.length();i+=2){
                 if(i==ret.length()-1){
-                    M.add(ret.charAt(i)*100);
+                    M.add(ret.charAt(i)*1000);
                     break;
                 }
-                M.add(ret.charAt(i)*100+ret.charAt(i+1));
+                M.add(ret.charAt(i)*1000+ret.charAt(i+1));
             }
-            System.out.println(M.toString());
         }catch (IOException e){
             e.printStackTrace();
         }finally {
@@ -97,35 +95,49 @@ public class RSA {
         return M;
     }
 
-    public static ArrayList<Integer> encode(ArrayList<Integer> M,BigInteger[] Pubkey){
-        ArrayList<Integer> C = new ArrayList<>();
+    public static void MtoString(ArrayList<Integer> R){
+        StringBuffer temp = new StringBuffer();
+        for(int i=0;i<R.size();i++){
+            if(i==(R.size()-1)){
+                if (R.get(i) % 1000 == 0) {
+                    temp.append((char) (R.get(i)/1000));
+                    break;
+                }
+            }
+            temp.append((char)(R.get(i)/1000));
+            temp.append((char)(R.get(i)%1000));
+        }
+        System.out.println(temp.toString());
+    }
+
+    public static ArrayList<BigInteger> encode(ArrayList<Integer> M,BigInteger[] Pubkey){
+        ArrayList<BigInteger> C = new ArrayList<>();
         int i = 0;
         for(int m : M){
-            C.add(quick_pow(m,Pubkey[0],Pubkey[1]));
+            C.add(quick_pow(BigInteger.valueOf(m),Pubkey[0],Pubkey[1]));
         }
         return C;
     }
 
-    public static ArrayList<Integer> decode(ArrayList<Integer> C,BigInteger[] Prikey){
+    public static ArrayList<Integer> decode(ArrayList<BigInteger> C,BigInteger[] Prikey){
         ArrayList<Integer> M = new ArrayList<>();
         int i = 0;
-        for(int c : C){
-            M.add(quick_pow(c,Prikey[0],Prikey[1]));
+        for(BigInteger c : C){
+            M.add(quick_pow(c,Prikey[0],Prikey[1]).intValue());
         }
         return M;
     }
 
-    public static int quick_pow(int m,BigInteger e,BigInteger n){
+    public static BigInteger quick_pow(BigInteger m,BigInteger e,BigInteger n){
         BigInteger ans = BigInteger.ONE;
-        BigInteger M = BigInteger.valueOf(m);
         while (e.compareTo(BigInteger.ZERO)!=0){
             if((e.and(BigInteger.ONE)).compareTo(BigInteger.ZERO)!=0){
-                ans=(ans.multiply(M)).mod(n);
+                ans=(ans.multiply(m)).mod(n);
             }
-            M=M.multiply(M).mod(n);
+            m=m.multiply(m).mod(n);
             e=e.shiftRight(1);
         }
-        return ans.intValue();
+        return ans;
     }
 
 
@@ -133,10 +145,11 @@ public class RSA {
         getKeyPair();
         ArrayList<Integer> M= readFile("lab2-Plaintext.txt");
         System.out.println("明文："+M.toString());
-        ArrayList<Integer> C = encode(M,PubKey);
+        ArrayList<BigInteger> C = encode(M,PubKey);
         System.out.println("密文:"+C.toString());
         ArrayList<Integer> R = decode(C,PriKey);
         System.out.println("解密:"+R.toString());
+        MtoString(R);
         return;
     }
 
